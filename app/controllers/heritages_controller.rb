2a0eb9b06bceb074b0eb32c32763f90c1b9b055b
masyu0039 <114735314+masyu0039@users.noreply.github.com>
2022-11-24 12:09:34 +0900

    first commit

--- app/controllers/heritages_controller.rb
+++ app/controllers/heritages_controller.rb
@@ -1,10 +1,12 @@
 class HeritagesController < ApplicationController
   before_action :authenticate_user!, except: [:index]
+  before_action :move_to_index, except: [:index, :new]
   def index
 
   end
 
   def new
-
+    @heritages = Heritage.new
   end
+
 end
