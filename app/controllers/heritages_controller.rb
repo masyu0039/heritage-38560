2e5009aae708661f6df2c0e0782be1d434bc856e
masyu0039 <114735314+masyu0039@users.noreply.github.com>
2022-12-03 21:38:00 +0900

    google.map api 変更

--- app/controllers/heritages_controller.rb
+++ app/controllers/heritages_controller.rb
@@ -2,11 +2,15 @@ class HeritagesController < ApplicationController
   before_action :authenticate_user!, except: [:index]
   before_action :move_to_index, except: [:index, :new]
   def index
-
+    @heritages = Heritage.all
   end
 
   def new
     @heritages = Heritage.new
+  end
+
+  def create 
+    
   end
 
 end
