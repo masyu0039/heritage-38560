d85c41cd4a7e60b7217e8a7981a68250cd4a80d2
masyu0039 <114735314+masyu0039@users.noreply.github.com>
2022-12-27 23:33:15 +0900

    ユーザー情報機能

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,8 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
+    @nickname = current_user.nickname
+    @heritages = current_user.heritages
+    @profile = current_user.profile
   end
 end
